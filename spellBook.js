3fab85889111c90ae752228a7c5846424baf3b45
Jason Jay <jaysonjay5@gmail.com>
2016-11-27 21:20:25 -0600

    Add files via upload

--- spellBook.js
+++ spellBook.js
@@ -2,21 +2,22 @@ function swapSpellsAndScrolls()
 {
 	var x = document.getElementById("leftPane");
 	
-	if(x.innerHTML == 'Scrolls')
+	if(x.innerHTML == 'Items')
 	{
 		x.innerHTML = "Spellbook";
 		scrollList();
 	}
 	else if(x.innerHTML == 'Spellbook')
 	{
-		x.innerHTML = "Scrolls";
+		x.innerHTML = "Items";
+		populateSpellBook();
 		spellList();
 	}
 }
 function updateText(event)
 {
 	var spellName = event.data.param1;
-	var spellLevel = event.data.param2;
+	var spellLevel = event.data.param2[0];
 	var spellSchool = event.data.param3;
 	
 	var maxSpellLevel = 0; for(var a of mage.mind)maxSpellLevel++;
@@ -29,12 +30,12 @@ function updateText(event)
 	    
 		for(var slot of buttonArray)
 		{
-			if(slot.className == "btn btn-info" && slot.textContent == "Empty")
+			if(slot.className == "btn-info" && slot.textContent == "Empty")
 			{	
 				slot.textContent = spellName;
 				return;
 			}
-			if(slot.className == "btn btn-warning" && slot.textContent == "Empty" && slot.id == spellSchool)
+			if(slot.className == "btn-warning" && slot.textContent == "Empty" && slot.id == spellSchool)
 			{
 				slot.textContent = spellName;
 				return;
@@ -61,18 +62,17 @@ function strike(specificScroll)
 
 function spellList()
 {
-    //rewrite this function to return only the spells currently in the characters spell book.
-	//the primary keys of the spells known will be in an array.
-
 	for(var num of characterSpellList)
 	{
 		if(masterSpellList[num].fields.level.includes("wizard"))
+		{
 			var levelOfSpell = masterSpellList[num].fields.level.split("wizard ");
+		}
 		else
 			continue;
 		
 		var addedSpell = $("<button></button>")
-		.attr("class","btn btn-primary")
+		.attr("class","btn-primary")
 		.attr("id",masterSpellList[num].fields.name)
 		.text(this.masterSpellList[num].fields.name);
 		//onclick parameters: Spell name, Level of Spell, School of spell
@@ -83,7 +83,6 @@ function spellList()
 		var levelSection = "#bookLevel"+levelOfSpell[1];
 		$(levelSection).append(addedSpell);
 	}	
-	populateMind();
 }
 
 function scrollList()
@@ -115,7 +114,14 @@ function populateMind()
 		{
 			
 			var addedButton=$("<button></button>")
-				.attr("class","btn btn-info")
+				.attr("class","btn-info")
+				.click(function()
+				{
+					if(($(this).text() != "Empty") && $(this).parent().attr("id") != "spellLevel0")
+					{
+						$(this).removeClass();$(this).addClass("btn-danger");
+					}
+				})
 				.text("Empty");
 			
 			$(levelToAddSlotsTo).append(addedButton);
@@ -123,11 +129,67 @@ function populateMind()
 	    if(mage.specialization != null && spellLevelCounter != 0)
 		{
 			var addedButton=$("<button></button>")
-				.attr("class","btn btn-warning")
+				.attr("class","btn-warning")
 				.attr("id",mage.specialization)
-				.text("Empty");
+				.text("Empty")
+				.click(function()
+				{
+					if($(this).text() != "Empty")
+					{
+						$(this).removeClass();$(this).addClass("btn-danger");
+					}
+				});
 			$(levelToAddSlotsTo).append(addedButton);
 		}
 		spellLevelCounter++;
 	}	
 }
+
+function populateSpellBook()
+{
+	var pane1Div = $("#pane1");
+	var container = $("<div></div>")
+					  .addClass("container-fluid");
+	
+	for(var a = 0; a < 10; a++)
+	{
+		var newRow = $("<div></div>")
+					.addClass("row");
+					
+		var newSpan = $("<div></div>")
+						.addClass("span5");
+						
+		var newButton = $("<button></button>") 
+						.attr("type","button") 
+						.addClass("btn-success") 
+						.attr("data-toggle","collapse") 
+						.attr("data-target","#bookLevel"+a)
+						.html("Level "+a);
+						
+		var newCollapseDiv =$("<div></div>")
+							.attr("id","bookLevel"+a)
+							.addClass("collapse in");
+		
+		newSpan.append(newButton);
+		newSpan.append(newCollapseDiv);
+		newRow.append(newSpan);
+		container.append(newRow);
+	}
+	
+	pane1Div.append(container);
+}
+
+function pageLoaded()
+{
+	spellList();
+	populateMind();
+}
+
+function testAddingSpell()
+{
+	
+	
+}
+
+
+
